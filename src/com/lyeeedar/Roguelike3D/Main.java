1bcdec7e4f76b1a470ef32ed76deba6277be5bac
Philip Collin <lyeeedar@gmail.com>
2013-02-22 10:34:31 +0000

    Test

--- src/com/lyeeedar/Roguelike3D/Main.java
+++ src/com/lyeeedar/Roguelike3D/Main.java
@@ -4,7 +4,6 @@ import com.lyeeedar.Roguelike3D.Game.GameData;
   
 public class Main {
 	public static void main(String[] args) {
-		
 		System.setProperty("org.lwjgl.opengl.Window.undecorated","true");
 		
 		GameData.game = new Roguelike3DGame();
